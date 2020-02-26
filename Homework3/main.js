caaa4e8dcdd499d6fd67e102d08429cf81a49b7a
AbundioMartinez <60721671+AbundioMartinez@users.noreply.github.com>
2020-02-26 17:37:38 -0500

    Add files via upload

---
+++ Homework3/main.js
@@ -0,0 +1,26 @@
+const changeShape = () => {
+    const square = document.getElementById("square");
+    if (square.style.backgroundColor === "purple") {
+      square.style.backgroundColor = "yellow";
+      square.style.borderRadius = "50%";
+      square.style.border = "px dotted blue";
+      document.body.style.backgroundColor = "white";
+    } else {
+      square.style.backgroundColor = "purple";
+      square.style.borderRadius = "0";
+      square.style.border = "px dashed #C8A2C8";
+      document.body.style.backgroundColor = "white";
+
+    }
+  };
+  
+
+  function clearBg() {
+    window.location.reload();
+  }
+  
+  const btnSquare = document.getElementById("btn-square");
+  const btnRefresh = document.getElementById("btn-refresh");
+  
+  btnSquare.addEventListener("click", changeShape);
+  btnRefresh.addEventListener("click", clearBg);
